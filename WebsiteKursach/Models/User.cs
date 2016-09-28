d979086c340c100c32676917517a948ca9c8c1af
Дмитрий Морозов < >
2016-09-28 21:53:46 +0300

    first commit

---
+++ WebsiteKursach/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebsiteKursach.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebsiteKursach/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteKursach.Startup))]
+namespace WebsiteKursach
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
